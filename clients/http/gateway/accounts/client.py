4b3ff5dbb8a6f6716e9be95502bb1421367efee6
natsts <leusheva_1996@mail.ru>
2026-01-28 18:04:29 +0500

    Практикуемся в использовании HTTP API-клиентов: выпуск физической карты

--- clients/http/gateway/accounts/client.py
+++ clients/http/gateway/accounts/client.py
@@ -3,6 +3,19 @@ from typing import TypedDict
 from httpx import Response, QueryParams
 
 from clients.http.client import HTTPClient
+from clients.http.gateway.cards.client import CardDict
+from clients.http.gateway.client import build_gateway_http_client
+
+
+class AccountDict(TypedDict):
+    """
+    Описание структуры аккаунта.
+    """
+    id: str
+    type: str
+    cards: list[CardDict]
+    status: str
+    balance: float
 
 
 class GetAccountsQueryDict(TypedDict):
@@ -12,11 +25,25 @@ class GetAccountsQueryDict(TypedDict):
     userId: str
 
 
+class GetAccountsResponseDict(TypedDict):
+    """
+    Описание структуры ответа получения списка счетов.
+    """
+    accounts: list[AccountDict]
+
+
 class OpenDepositAccountRequestDict(TypedDict):
     """
     Структура данных для открытия депозитного счета.
     """
     userId: str
+
+
+class OpenDepositAccountResponseDict(TypedDict):
+    """
+    Описание структуры ответа открытия депозитного счета.
+    """
+    account: AccountDict
 
 
 class OpenSavingsAccountRequestDict(TypedDict):
@@ -26,11 +53,25 @@ class OpenSavingsAccountRequestDict(TypedDict):
     userId: str
 
 
+class OpenSavingsAccountResponseDict(TypedDict):
+    """
+    Описание структуры ответа открытия сберегательного счета.
+    """
+    account: AccountDict
+
+
 class OpenDebitCardAccountRequestDict(TypedDict):
     """
     Структура данных для открытия дебетового счета.
     """
     userId: str
+
+
+class OpenDebitCardAccountResponseDict(TypedDict):
+    """
+    Описание структуры ответа открытия дебетового счета.
+    """
+    account: AccountDict
 
 
 class OpenCreditCardAccountRequestDict(TypedDict):
@@ -40,12 +81,19 @@ class OpenCreditCardAccountRequestDict(TypedDict):
     userId: str
 
 
+class OpenCreditCardAccountResponseDict(TypedDict):
+    """
+    Описание структуры ответа открытия кредитного счета.
+    """
+    account: AccountDict
+
+
 class AccountsGatewayHTTPClient(HTTPClient):
     """
     Клиент для взаимодействия с /api/v1/accounts сервиса http-gateway.
     """
 
-    def get_accounts_api(self, query: GetAccountsQueryDict) -> Response:
+    def get_accounts_api(self, query: GetAccountsQueryDict):
         """
         Выполняет GET-запрос на получение списка счетов пользователя.
 
@@ -89,3 +137,37 @@ class AccountsGatewayHTTPClient(HTTPClient):
         :return: Объект httpx.Response.
         """
         return self.post("/api/v1/accounts/open-credit-card-account", json=request)
+
+    def get_accounts(self, user_id: str) -> GetAccountsResponseDict:
+        query = GetAccountsQueryDict(userId=user_id)
+        response = self.get_accounts_api(query)
+        return response.json()
+
+    def open_deposit_account(self, user_id: str) -> OpenDepositAccountResponseDict:
+        request = OpenDepositAccountRequestDict(userId=user_id)
+        response = self.open_deposit_account_api(request)
+        return response.json()
+
+    def open_savings_account(self, user_id: str) -> OpenSavingsAccountResponseDict:
+        request = OpenSavingsAccountRequestDict(userId=user_id)
+        response = self.open_savings_account_api(request)
+        return response.json()
+
+    def open_debit_card_account(self, user_id: str) -> OpenDebitCardAccountResponseDict:
+        request = OpenDebitCardAccountRequestDict(userId=user_id)
+        response = self.open_debit_card_account_api(request)
+        return response.json()
+
+    def open_credit_card_account(self, user_id: str) -> OpenCreditCardAccountResponseDict:
+        request = OpenCreditCardAccountRequestDict(userId=user_id)
+        response = self.open_credit_card_account_api(request)
+        return response.json()
+
+
+def build_accounts_gateway_http_client() -> AccountsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр AccountsGatewayHTTPClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию AccountsGatewayHTTPClient.
+    """
+    return AccountsGatewayHTTPClient(client=build_gateway_http_client())
--- clients/http/gateway/users/client.py
+++ clients/http/gateway/users/client.py
@@ -1,8 +1,29 @@
+import time
 from typing import TypedDict
 
 from httpx import Response
 
 from clients.http.client import HTTPClient
+from clients.http.gateway.client import build_gateway_http_client
+
+
+class UserDict(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+    phoneNumber: str
+
+
+class GetUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа получения пользователя.
+    """
+    user: UserDict
 
 
 class CreateUserRequestDict(TypedDict):
@@ -14,6 +35,13 @@ class CreateUserRequestDict(TypedDict):
     firstName: str
     middleName: str
     phoneNumber: str
+
+
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserDict
 
 
 class UsersGatewayHTTPClient(HTTPClient):
@@ -28,7 +56,7 @@ class UsersGatewayHTTPClient(HTTPClient):
         :param user_id: Идентификатор пользователя.
         :return: Ответ от сервера (объект httpx.Response).
         """
-        return self.get(url=f'/api/v1/users/{user_id}')
+        return self.get(f"/api/v1/users/{user_id}")
 
     def create_user_api(self, request: CreateUserRequestDict) -> Response:
         """
@@ -38,3 +66,28 @@ class UsersGatewayHTTPClient(HTTPClient):
         :return: Ответ от сервера (объект httpx.Response).
         """
         return self.post("/api/v1/users", json=request)
+
+    def get_user(self, user_id: str) -> GetUserResponseDict:
+        response = self.get_user_api(user_id)
+        return response.json()
+
+    def create_user(self) -> CreateUserResponseDict:
+        request = CreateUserRequestDict(
+            email=f"user.{time.time()}@example.com",
+            lastName="string",
+            firstName="string",
+            middleName="string",
+            phoneNumber="string"
+        )
+        response = self.create_user_api(request)
+        return response.json()
+
+
+def build_users_gateway_http_client() -> UsersGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр UsersGatewayHTTPClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию UsersGatewayHTTPClient.
+    """
+    return UsersGatewayHTTPClient(client=build_gateway_http_client())
+
