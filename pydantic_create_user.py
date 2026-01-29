418a88eedcac458fdcbf932d510b13166ee4da4c
natsts <leusheva_1996@mail.ru>
2026-01-29 14:09:17 +0500

    Знакомство с Pydantic.Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserSchema(BaseModel):
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserRequestSchema(BaseModel):
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserResponseSchema(BaseModel):
+    user: UserSchema
